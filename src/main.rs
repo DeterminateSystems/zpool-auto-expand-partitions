08cd81869d0413f564edd17a3196683f5ade6951
Kayla Firestack <dev@kaylafire.me>
2022-02-24 15:15:22 +0000

    main.rs: add clap derive zpool option

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Options {
+    zpool_name: String,
+}
+
 fn main() {
+    let options = Options::parse();
     let mut lzfs = libzfs::libzfs::Libzfs::new();
 
-    let pool = lzfs.pool_by_name("tank").expect("expected pool");
+    let pool = lzfs.pool_by_name(&options.zpool_name).expect("Pool retreval failed");
+
     dbg!(pool.vdev_tree());
 }
