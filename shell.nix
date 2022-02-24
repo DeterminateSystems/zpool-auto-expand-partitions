363ee46be781c1b3e240c0fa46d78b37da980943
Kayla Firestack <dev@kaylafire.me>
2022-02-24 15:15:42 +0000

    shell.nix: add git and gitui, reorganize

--- shell.nix
+++ shell.nix
@@ -2,8 +2,11 @@ let pkgs = import <nixpkgs> {}; in pkgs.stdenv.mkDerivation {
 	pname = "ztest";
 	version = "0.0.0";
 	buildInputs = [
-		pkgs.zfs.dev
-		pkgs.pkg-config 
+		pkgs.git pkgs.gitui
+		
 		pkgs.cargo pkgs.rustc pkgs.gcc pkgs.cargo-edit
+
+		pkgs.pkg-config 
+		pkgs.zfs.dev
 	];
 }
