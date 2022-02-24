5e6cfa1ceab80be926f971e4d78ba59822ace546
Kayla Firestack <dev@kaylafire.me>
2022-02-24 15:52:21 +0000

    add blkid, add matches! for vdev::disk

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,27 @@ fn main() {
     let mut lzfs = libzfs::libzfs::Libzfs::new();
 
     let pool = lzfs.pool_by_name(&options.zpool_name).expect("Pool retreval failed");
+    let cache = blkid::cache::Cache::new().expect("Failed to open blkid cache");
     match pool.vdev_tree() {
         Ok(vdev) => vdev_process(vdev),
         Err(e) => println!("Failed: {e}")
     };
 }
 
-fn vdev_process(vdev: libzfs::vdev::VDev) {
-    
+fn vdev_process(vdev: libzfs::vdev::VDev, cache: &blkid::cache::Cache) {
+    use libzfs::vdev::VDev;
+    match vdev {
+        VDev::Root { children, spares, cache } => {
+            let a = children.iter().chain(spares.iter()).chain(cache.iter()).for_each(|v| vdev_disks(v, cache));
+        },
+        _ => {},
+    };
+}
+
+fn vdev_disks(vdev: &libzfs::vdev::VDev, cache: &blkid::cache::Cache) {
+    use libzfs::vdev::VDev;
+    if matches!(vdev, VDev::Disk { whole_disk: Some(false), .. }) {
+        dbg!(vdev);
+
+    }
 }
