37a606365726a11ee6668faa63c6a5165414e2f1
Kayla Firestack <dev@kaylafire.me>
2022-02-25 20:40:41 +0000

    main.rs: commit state of disarray

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     let pool = lzfs.pool_by_name(&options.zpool_name).expect("Pool retreval failed");
     let cache = blkid::cache::Cache::new().expect("Failed to open blkid cache");
     match pool.vdev_tree() {
-        Ok(vdev) => vdev_process(vdev),
+        Ok(vdev) => vdev_process(vdev, &cache),
         Err(e) => println!("Failed: {e}")
     };
 }
@@ -23,8 +23,11 @@ fn main() {
 fn vdev_process(vdev: libzfs::vdev::VDev, cache: &blkid::cache::Cache) {
     use libzfs::vdev::VDev;
     match vdev {
-        VDev::Root { children, spares, cache } => {
-            let a = children.iter().chain(spares.iter()).chain(cache.iter()).for_each(|v| vdev_disks(v, cache));
+        VDev::Root { children, .. } => {
+            let a = children.iter()
+                // .chain(spares.iter())
+                // .chain(cache.iter())
+                .for_each(|v| vdev_disks(v, cache));
         },
         _ => {},
     };
@@ -32,8 +35,47 @@ fn vdev_process(vdev: libzfs::vdev::VDev, cache: &blkid::cache::Cache) {
 
 fn vdev_disks(vdev: &libzfs::vdev::VDev, cache: &blkid::cache::Cache) {
     use libzfs::vdev::VDev;
-    if matches!(vdev, VDev::Disk { whole_disk: Some(false), .. }) {
-        dbg!(vdev);
+    match vdev {
+        VDev::Disk { path, whole_disk: Some(false), state, .. } if state == "ONLINE" => {
+            // dbg!(path);
+            dbg!(&vdev);
+            dbg!(path.file_name());
+            dbg!({
+                let mut p: std::path::PathBuf = [ "/sys/class/block" ].iter().collect();
+                p.push(path.file_name().unwrap());
+                p.push("partition");
+                std::fs::File::open(p)
+            });
+            dbg!(cache.get_dev(path.to_str().unwrap(), blkid::dev::GetDevFlags::empty()).unwrap().name());
 
+            use blkid::prober::{Prober, ProbeState};
+            let prober = Prober::new_from_filename(path).unwrap();
+            prober.enable_partitions(true);
+
+            if matches!(prober.do_full_probe(), Ok(ProbeState::Success)) {
+                dbg!(prober.lookup_value("TYPE"));
+                dbg!(prober.lookup_value("PTTYPE"));
+                let ptlist = prober
+                    .part_list();
+                dbg!(ptlist.as_ref().err());
+                dbg!(
+                        ptlist.ok().and_then(|v| v.get_table())
+                        // .and_then(|v| v.get_parent())
+                );
+            }
+
+        },
+        _ => {},
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        vdev_disks()
     }
 }
