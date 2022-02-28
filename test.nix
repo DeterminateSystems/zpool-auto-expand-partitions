c89921a538b28cf7d6406a3d4a4cea1dbe88b257
Kayla Firestack <dev@kaylafire.me>
2022-02-28 15:39:50 -0500

    add test.nix

---
+++ test.nix
@@ -0,0 +1,33 @@
+import <nixpkgs/nixos/tests/make-test-python.nix> ({ pkgs, ... }:
+  {
+    name = "multi-disk-zfs";
+    nodes = {
+      machine =
+        { pkgs, ... }: {
+          environment.systemPackages = [ pkgs.parted pkgs.cloud-utils ];
+          boot.supportedFilesystems = [ "zfs" ];
+          networking.hostId = "00000000";
+
+          # nixos config ...
+          virtualisation = {
+            emptyDiskImages = [ 20480 20480 20480 20480 20480 20480 ];
+          };
+        };
+    };
+
+    testScript = { nodes, ... }:
+      ''
+        start_all()
+        machine.wait_for_unit("default.target")
+        print(machine.succeed('mount'))
+
+        print(machine.succeed('parted --script /dev/vdb -- mklabel gpt'))
+        print(machine.succeed('parted --script /dev/vdb -- mkpart primary 1M 70M'))
+
+        print(machine.succeed('parted --script /dev/vdc -- mklabel gpt'))
+        print(machine.succeed('parted --script /dev/vdc -- mkpart primary 1M 70M'))
+
+        print(machine.succeed('zpool create tank mirror /dev/vdb1 /dev/vdc1 mirror /dev/vdd /dev/vde mirror /dev/vdf /dev/vdg'))
+        print(machine.succeed('zpool list -v'))
+      '';
+  })
