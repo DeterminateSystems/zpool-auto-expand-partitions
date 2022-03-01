82635ff32dbabef62e95f88f5f94dc2df5f999be
Kayla Firestack <dev@kaylafire.me>
2022-03-01 14:06:04 -0500

    nix: run nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -36,7 +36,9 @@
             codespell
             nixpkgs-fmt
             rustfmt
-            git gitui cloud-utils
+            git
+            gitui
+            cloud-utils
             # llvm cargo-edit rustfmt clippy
           ];
         });
@@ -44,40 +46,42 @@
       # devShell = self.packages;
 
       packages = forAllSystems
-        ({ system, pkgs, ... }: let lib = pkgs.lib; in
+        ({ system, pkgs, ... }:
+          let lib = pkgs.lib; in
           {
-            zpool_part_disks = let cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml); in pkgs.rustPlatform.buildRustPackage rec {
-              pname = cargo.package.name;
-              version = cargo.package.version;
+            zpool_part_disks = let cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml); in
+              pkgs.rustPlatform.buildRustPackage rec {
+                pname = cargo.package.name;
+                version = cargo.package.version;
 
-              src = self;
+                src = self;
 
-              cargoLock.lockFile = ./Cargo.lock;
+                cargoLock.lockFile = ./Cargo.lock;
 
 
-              # https://hoverbear.org/blog/rust-bindgen-in-nix/
-              LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
-              C_INCLUDE_PATH = lib.makeSearchPathOutput "dev" "include" [ 
-                pkgs.util-linux
-              ];
-              BINDGEN_EXTRA_CLANG_ARGS = lib.concatStringsSep " " [
-                (builtins.readFile "${pkgs.stdenv.cc}/nix-support/libc-crt1-cflags")
-                (builtins.readFile "${pkgs.stdenv.cc}/nix-support/libc-cflags")
-                (builtins.readFile "${pkgs.stdenv.cc}/nix-support/cc-cflags")
-                (lib.optionalString pkgs.stdenv.cc.isGNU ( lib.concatStringsSep " " [
-                  "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${lib.getVersion pkgs.stdenv.cc.cc}"
-                  "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${lib.getVersion pkgs.stdenv.cc.cc}/${pkgs.stdenv.hostPlatform.config}"
-                  "-idirafter ${pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${lib.getVersion pkgs.stdenv.cc.cc}/include"
-                ]))
-              ];
+                # https://hoverbear.org/blog/rust-bindgen-in-nix/
+                LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+                C_INCLUDE_PATH = lib.makeSearchPathOutput "dev" "include" [
+                  pkgs.util-linux
+                ];
+                BINDGEN_EXTRA_CLANG_ARGS = lib.concatStringsSep " " [
+                  (builtins.readFile "${pkgs.stdenv.cc}/nix-support/libc-crt1-cflags")
+                  (builtins.readFile "${pkgs.stdenv.cc}/nix-support/libc-cflags")
+                  (builtins.readFile "${pkgs.stdenv.cc}/nix-support/cc-cflags")
+                  (lib.optionalString pkgs.stdenv.cc.isGNU (lib.concatStringsSep " " [
+                    "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${lib.getVersion pkgs.stdenv.cc.cc}"
+                    "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${lib.getVersion pkgs.stdenv.cc.cc}/${pkgs.stdenv.hostPlatform.config}"
+                    "-idirafter ${pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${lib.getVersion pkgs.stdenv.cc.cc}/include"
+                  ]))
+                ];
 
-              nativeBuildInputs = [ pkgs.pkg-config ];
-              buildInputs = [
-                pkgs.zfs.dev
-                pkgs.util-linux.dev
-              ];
+                nativeBuildInputs = [ pkgs.pkg-config ];
+                buildInputs = [
+                  pkgs.zfs.dev
+                  pkgs.util-linux.dev
+                ];
 
-            };
+              };
           });
 
       defaultPackage = forAllSystems ({ system, ... }: self.packages.${system}.zpool_part_disks);
