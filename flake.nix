b661a2c02e63d275aee3137c99e84892278de7de
Cole Helbling <cole.helbling@determinate.systems>
2022-03-03 11:17:31 -0800

    flake: fix devShell
    The environment variables are necessary to build the package, and
    `inputsFrom` doesn't take these into account. Split them out into a
    helper so that both the package build and the devShell can use them.

--- flake.nix
+++ flake.nix
@@ -22,35 +22,49 @@
         inherit system;
         pkgs = import nixpkgs { inherit system; };
       });
+
+      inherit (nixpkgs)
+        lib;
+
+      bindgenEnvs = pkgs: {
+        # https://hoverbear.org/blog/rust-bindgen-in-nix/
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        C_INCLUDE_PATH = lib.makeSearchPathOutput "dev" "include" [ pkgs.util-linux ];
+        BINDGEN_EXTRA_CLANG_ARGS = lib.concatStringsSep " " [
+          (builtins.readFile "${pkgs.stdenv.cc}/nix-support/libc-crt1-cflags")
+          (builtins.readFile "${pkgs.stdenv.cc}/nix-support/libc-cflags")
+          (builtins.readFile "${pkgs.stdenv.cc}/nix-support/cc-cflags")
+          (lib.optionalString pkgs.stdenv.cc.isGNU (lib.concatStringsSep " " [
+            "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${lib.getVersion pkgs.stdenv.cc.cc}"
+            "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${lib.getVersion pkgs.stdenv.cc.cc}/${pkgs.stdenv.hostPlatform.config}"
+            "-idirafter ${pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${lib.getVersion pkgs.stdenv.cc.cc}/include"
+          ]))
+        ];
+      };
     in
     {
       devShell = forAllSystems ({ system, pkgs, ... }:
-        pkgs.mkShell {
+        pkgs.mkShell ({
           name = "zpool_part_disks";
 
           inputsFrom = [
             self.packages.${system}.zpool_part_disks
           ];
+
           buildInputs = with pkgs; [
             cargo
             codespell
             nixpkgs-fmt
             rustfmt
             git
-            gitui
-            cloud-utils
-            # llvm cargo-edit rustfmt clippy
           ];
-        });
-
-      # devShell = self.packages;
+        } // bindgenEnvs pkgs));
 
       packages = forAllSystems
         ({ system, pkgs, ... }:
-          let lib = pkgs.lib; in
           {
             zpool_part_disks = let cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml); in
-              pkgs.rustPlatform.buildRustPackage rec {
+              pkgs.rustPlatform.buildRustPackage (rec {
                 pname = cargo.package.name;
                 version = cargo.package.version;
 
@@ -58,30 +72,15 @@
 
                 cargoLock.lockFile = ./Cargo.lock;
 
-
-                # https://hoverbear.org/blog/rust-bindgen-in-nix/
-                LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-                C_INCLUDE_PATH = lib.makeSearchPathOutput "dev" "include" [
-                  pkgs.util-linux
-                ];
-                BINDGEN_EXTRA_CLANG_ARGS = lib.concatStringsSep " " [
-                  (builtins.readFile "${pkgs.stdenv.cc}/nix-support/libc-crt1-cflags")
-                  (builtins.readFile "${pkgs.stdenv.cc}/nix-support/libc-cflags")
-                  (builtins.readFile "${pkgs.stdenv.cc}/nix-support/cc-cflags")
-                  (lib.optionalString pkgs.stdenv.cc.isGNU (lib.concatStringsSep " " [
-                    "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${lib.getVersion pkgs.stdenv.cc.cc}"
-                    "-isystem ${pkgs.stdenv.cc.cc}/include/c++/${lib.getVersion pkgs.stdenv.cc.cc}/${pkgs.stdenv.hostPlatform.config}"
-                    "-idirafter ${pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${lib.getVersion pkgs.stdenv.cc.cc}/include"
-                  ]))
+                nativeBuildInputs = [
+                  pkgs.pkg-config
                 ];
 
-                nativeBuildInputs = [ pkgs.pkg-config ];
                 buildInputs = [
                   pkgs.zfs.dev
                   pkgs.util-linux.dev
                 ];
-
-              };
+              } // bindgenEnvs pkgs);
           });
 
       defaultPackage = forAllSystems ({ system, ... }: self.packages.${system}.zpool_part_disks);
